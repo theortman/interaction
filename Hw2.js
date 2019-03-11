180df8e6d3fc8be025a117edb1b2426beb4c61d5
theortman <metwesttieno@gmail.com>
2019-03-11 01:53:18 -0700

    Update Hw2.js

--- Hw2.js
+++ Hw2.js
@@ -68,7 +68,7 @@ function create ()
 
     cursors = this.input.keyboard.createCursorKeys();
 
-    player = this.physics.add.image(400, 100, 'crate').setScale(3);
+    player = this.physics.add.image(400, 100, 'crate').setScale(100);
 
     player.setImmovable();
     player.setCollideWorldBounds(true);
